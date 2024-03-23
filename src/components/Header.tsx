06a8372af1b9b7200de5b93964008e7b76c3bc1b
Xiaosong Bi <bixiaosong@gmail.com>
2024-03-23 15:37:46 +0100

    The import of bootstrap.scss was causing a compilation error because the file could not be found. This commit removes the import statement from main.scss. The project builds successfully after this change, indicating that the import was not necessary.

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,67 +1,67 @@
-import { Link } from "react-router-dom";
-import { IMAGES } from "../constent/theme";
-import Menu from "./Menu";
-import { useContext, useEffect, useRef, useState } from "react";
-import { Context } from "../context/AppContext";
+import { Link } from 'react-router-dom'
+import { IMAGES } from '../constent/theme'
+import Menu from './Menu'
+import { useContext, useEffect, useRef, useState } from 'react'
+import { Context } from '../context/AppContext'
 
 const Header = () => {
   const { headerClass, setShowSignInForm, headerSidebar, setHeaderSidebar } =
-    useContext(Context);
-  const [cart, setCart] = useState<boolean>(false);
-  const cartRef = useRef<HTMLLIElement[]>([]);
-  const [scroll, setScroll] = useState<boolean>(false);
+    useContext(Context)
+  const [cart, setCart] = useState<boolean>(false)
+  const cartRef = useRef<HTMLLIElement[]>([])
+  const [scroll, setScroll] = useState<boolean>(false)
 
   const cartButton = () => {
-    setCart(!cart);
-  };
+    setCart(!cart)
+  }
   const deletItems = (ind: number) => {
-    cartRef.current[ind].classList.add("cartListItems");
+    cartRef.current[ind].classList.add('cartListItems')
     setTimeout(() => {
-      cartRef.current[ind].remove();
-    }, 500);
-  };
+      cartRef.current[ind].remove()
+    }, 500)
+  }
 
   const scrollHandler = () => {
     if (window.scrollY > 80) {
-      setScroll(true);
+      setScroll(true)
     } else {
-      setScroll(false);
+      setScroll(false)
     }
-  };
+  }
 
   useEffect(() => {
-    window.addEventListener("scroll", scrollHandler);
+    window.addEventListener('scroll', scrollHandler)
     return () => {
-      window.removeEventListener("scroll", scrollHandler);
-    };
-  }, []);
+      window.removeEventListener('scroll', scrollHandler)
+    }
+  }, [])
 
   return (
-    <header className="site-header mo-left header header-transparent transparent-white style-1">
+    <header className='site-header mo-left header header-transparent transparent-white style-1'>
       <div
         className={`sticky-header main-bar-wraper navbar-expand-lg ${
-          scroll ? "is-fixed" : ""
+          scroll ? 'is-fixed' : ''
         }`}
       >
-        <div className="main-bar clearfix ">
-          <div className="container clearfix">
-            <div className="logo-header mostion">
-              <div className="logo-header mostion">
+        <div className='main-bar clearfix '>
+          <div className='container clearfix'>
+            <div className='logo-header mostion'>
+              <div className='logo-header mostion'>
                 {headerClass ? (
                   <>
                     {scroll ? (
-                      <Link to="/" className="anim-logo">
-                        <img src={IMAGES.logo} alt="/" />
+                      <Link to='/' className='anim-logo'>
+                        <img src={IMAGES.logo} alt='/' />
                       </Link>
                     ) : (
-                      <Link to="/" className="anim-logo-white">
-                        <img src={IMAGES.logo2} alt="/" />
+                      <Link to='/' className='anim-logo-white'>
+                        <img src={IMAGES.logo2} alt='/' />
                       </Link>
                     )}
                   </>
                 ) : (
-                  <Link to="/" className="anim-logo">
-                    <img src={IMAGES.logo} alt="/" />
+                  <Link to='/' className='anim-logo'>
+                    <img src={IMAGES.logo} alt='/' />
                   </Link>
                 )}
               </div>
@@ -69,11 +69,11 @@ const Header = () => {
 
             <button
               className={`navbar-toggler collapsed navicon justify-content-end ${
-                headerSidebar ? "open" : ""
+                headerSidebar ? 'open' : ''
               }`}
-              type="button"
+              type='button'
               onClick={() => {
-                setHeaderSidebar(!headerSidebar);
+                setHeaderSidebar(!headerSidebar)
               }}
             >
               <span></span>
@@ -81,76 +81,76 @@ const Header = () => {
               <span></span>
             </button>
 
-            <div className="extra-nav">
-              <div className="extra-cell">
+            <div className='extra-nav'>
+              <div className='extra-cell'>
                 <ul>
                   <li>
                     <Link
-                      className="btn btn-white btn-square btn-shadow"
-                      to={"#"}
+                      className='btn btn-white btn-square btn-shadow'
+                      to={'#'}
                       onClick={() => {
-                        setShowSignInForm(true);
+                        setShowSignInForm(true)
                       }}
                     >
-                      <i className="flaticon-user"></i>
+                      <i className='flaticon-user'></i>
                     </Link>
                   </li>
                   <li>
                     <button
-                      type="button"
-                      className="btn btn-white btn-square btn-shadow cart-btn"
+                      type='button'
+                      className='btn btn-white btn-square btn-shadow cart-btn'
                       onClick={cartButton}
                     >
-                      <i className="flaticon-shopping-bag-1"></i>
-                      <span className="badge">6</span>
+                      <i className='flaticon-shopping-bag-1'></i>
+                      <span className='badge'>6</span>
                     </button>
                     <div
                       style={{
-                        transition: "all 0.5s",
-                        opacity: cart ? "1" : "0",
+                        transition: 'all 0.5s',
+                        opacity: cart ? '1' : '0',
                       }}
                     >
                       <ul
-                        className="dropdown-menu cart-list"
+                        className='dropdown-menu cart-list'
                         style={{
-                          display: cart ? "block" : "",
-                          transitionDuration: "0.5s",
-                          opacity: cart ? "1" : "0",
-                          overflow: "hidden",
+                          display: cart ? 'block' : '',
+                          transitionDuration: '0.5s',
+                          opacity: cart ? '1' : '0',
+                          overflow: 'hidden',
                         }}
                       >
                         <li
-                          className="cart-item"
+                          className='cart-item'
                           ref={(node) => {
                             if (node) {
-                              cartRef.current.push(node);
+                              cartRef.current.push(node)
                             }
                           }}
                         >
-                          <div className="media">
-                            <div className="media-left">
-                              <Link to="/product-detail">
+                          <div className='media'>
+                            <div className='media-left'>
+                              <Link to='/product-detail'>
                                 <img
-                                  alt="/"
-                                  className="media-object"
+                                  alt='/'
+                                  className='media-object'
                                   src={IMAGES.shop_pic2}
                                 />
                               </Link>
                             </div>
-                            <div className="media-body">
-                              <h6 className="dz-title">
+                            <div className='media-body'>
+                              <h6 className='dz-title'>
                                 <Link
-                                  to="/product-detail"
-                                  className="media-heading"
+                                  to='/product-detail'
+                                  className='media-heading'
                                 >
                                   Double Burger
                                 </Link>
                               </h6>
-                              <span className="dz-price">$28.00</span>
+                              <span className='dz-price'>$28.00</span>
                               <span
-                                className="item-close"
+                                className='item-close'
                                 onClick={() => {
-                                  deletItems(0);
+                                  deletItems(0)
                                 }}
                               >
                                 &times;
@@ -159,37 +159,37 @@ const Header = () => {
                           </div>
                         </li>
                         <li
-                          className="cart-item"
+                          className='cart-item'
                           ref={(node) => {
                             if (node) {
-                              cartRef.current.push(node);
+                              cartRef.current.push(node)
                             }
                           }}
                         >
-                          <div className="media">
-                            <div className="media-left">
-                              <Link to="/product-detail">
+                          <div className='media'>
+                            <div className='media-left'>
+                              <Link to='/product-detail'>
                                 <img
-                                  alt="/"
-                                  className="media-object"
+                                  alt='/'
+                                  className='media-object'
                                   src={IMAGES.shop_pic3}
                                 />
                               </Link>
                             </div>
-                            <div className="media-body">
-                              <h6 className="dz-title">
+                            <div className='media-body'>
+                              <h6 className='dz-title'>
                                 <Link
-                                  to="/product-detail"
-                                  className="media-heading"
+                                  to='/product-detail'
+                                  className='media-heading'
                                 >
                                   Cheese Burger
                                 </Link>
                               </h6>
-                              <span className="dz-price">$20.00</span>
+                              <span className='dz-price'>$20.00</span>
                               <span
-                                className="item-close"
+                                className='item-close'
                                 onClick={() => {
-                                  deletItems(1);
+                                  deletItems(1)
                                 }}
                               >
                                 &times;
@@ -198,37 +198,37 @@ const Header = () => {
                           </div>
                         </li>
                         <li
-                          className="cart-item"
+                          className='cart-item'
                           ref={(node) => {
                             if (node) {
-                              cartRef.current.push(node);
+                              cartRef.current.push(node)
                             }
                           }}
                         >
-                          <div className="media">
-                            <div className="media-left">
-                              <Link to="/product-detail">
+                          <div className='media'>
+                            <div className='media-left'>
+                              <Link to='/product-detail'>
                                 <img
-                                  alt="/"
-                                  className="media-object"
+                                  alt='/'
+                                  className='media-object'
                                   src={IMAGES.shop_pic4}
                                 />
                               </Link>
                             </div>
-                            <div className="media-body">
-                              <h6 className="dz-title">
+                            <div className='media-body'>
+                              <h6 className='dz-title'>
                                 <Link
-                                  to="/product-detail"
-                                  className="media-heading"
+                                  to='/product-detail'
+                                  className='media-heading'
                                 >
                                   Burger
                                 </Link>
                               </h6>
-                              <span className="dz-price">$15.00</span>
+                              <span className='dz-price'>$15.00</span>
                               <span
-                                className="item-close"
+                                className='item-close'
                                 onClick={() => {
-                                  deletItems(2);
+                                  deletItems(2)
                                 }}
                               >
                                 &times;
@@ -236,20 +236,20 @@ const Header = () => {
                             </div>
                           </div>
                         </li>
-                        <li className="cart-item text-center d-flex justify-content-between">
-                          <h6 className="text-primary mb-0">Total:</h6>
-                          <h6 className="text-primary mb-0">$63</h6>
+                        <li className='cart-item text-center d-flex justify-content-between'>
+                          <h6 className='text-primary mb-0'>Total:</h6>
+                          <h6 className='text-primary mb-0'>$63</h6>
                         </li>
-                        <li className="text-center d-flex">
+                        <li className='text-center d-flex'>
                           <Link
-                            to="/shop-cart"
-                            className="btn btn-primary me-2 w-100 d-block btn-hover-1"
+                            to='/shop-cart'
+                            className='btn btn-primary me-2 w-100 d-block btn-hover-1'
                           >
                             <span>View Cart</span>
                           </Link>
                           <Link
-                            to="/our-menu-1"
-                            className="btn btn-outline-primary w-100 d-block btn-hover-1"
+                            to='/our-menu-1'
+                            className='btn btn-outline-primary w-100 d-block btn-hover-1'
                           >
                             <span>Menu</span>
                           </Link>
@@ -263,18 +263,18 @@ const Header = () => {
 
             <div
               className={`header-nav navbar-collapse justify-content-end ${
-                headerSidebar ? "show" : ""
+                headerSidebar ? 'show' : ''
               }`}
-              id="navbarNavDropdown"
+              id='navbarNavDropdown'
             >
-              {" "}
+              {' '}
               <Menu />
             </div>
           </div>
         </div>
       </div>
     </header>
-  );
-};
+  )
+}
 
-export default Header;
+export default Header
